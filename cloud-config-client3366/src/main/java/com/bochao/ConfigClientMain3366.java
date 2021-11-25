b5bdacc09a093fcf2e700af240f4082cf9a5465b
“wujunjie” <1035626172@qq.com>
2021-11-25 17:24:24 +0800

    config配置中心

--- cloud-config-client3366/src/main/java/com/bochao/ConfigClientMain3366.java
+++ cloud-config-client3366/src/main/java/com/bochao/ConfigClientMain3366.java
@@ -2,14 +2,13 @@ package com.bochao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain3355 {
+public class ConfigClientMain3366 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain3355.class, args);
+        SpringApplication.run(ConfigClientMain3366.class, args);
     }
 }
